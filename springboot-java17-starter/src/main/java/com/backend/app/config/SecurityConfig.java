73811c4036aedec716a73d44e44b0687fec3e0ce
David Cañete <daviddanielc25@gmail.com>
2024-07-25 19:29:29 -0300

    Configuracion sin anotaciones en el SecurityFilterChain

--- springboot-java17-starter/src/main/java/com/backend/app/config/SecurityConfig.java
+++ springboot-java17-starter/src/main/java/com/backend/app/config/SecurityConfig.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,7 +31,25 @@ public class SecurityConfig {
     
     @Bean
     public SecurityFilterChain securtyFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.build();
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                // Cuando solo nos loguamos con usuario y contraseña (sin token)
+                .httpBasic(Customizer.withDefaults())
+                // Para aplicaciones web lo ideal es trabajar sin estado (sin guardar el estado, ya que es un objeto bastante pesado)
+                // En su lugar se le da un token que tiene un tiempo determinado de uso o cantidad limita de usos
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    // Configurar los endpoints publicos
+                    http.requestMatchers(HttpMethod.GET, "/api/users").permitAll();
+
+                    // Configuarar los endpoints privados
+                    http.requestMatchers(HttpMethod.POST, "/api/users").hasAuthority("READ");
+
+                    // Configurar el resto de endpoints - NO ESPECIFICADOS
+                    http.anyRequest().denyAll(); 
+                    //http.anyRequest().authenticated(); --> Si no especifico pero si tengo los permisos me permite acceder
+                })
+                .build();
     }
 
     @Bean 
@@ -39,8 +60,8 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(userDetailsService());
-        provider.setUserDetailsService(passwordEncoder());
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
         return provider;
     }
 
